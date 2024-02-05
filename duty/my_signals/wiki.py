6f5a07ee8753df5eb2ff05919d68036160541bbe
Aze <152387619+Azeget@users.noreply.github.com>
2024-02-05 18:05:17 +0300

    Create wiki.py

---
+++ duty/my_signals/wiki.py
@@ -0,0 +1,21 @@
+from duty.objects import dp, MySignalEvent
+from duty.utils import find_mention_by_event
+import wikipedia
+
+@dp.my_signal_event_register('вики')
+def wiki_command(event: MySignalEvent) -> str:
+    user_id = event.obj.from_id  
+    peer_id = event.chat_id if event.is_chat else user_id  
+    query = event.payload.get('text')  
+
+    if query:
+        query = query.strip() 
+        try:
+            wiki_summary = wikipedia.summary(query, sentences=3)  
+            event.api.messages.send(peer_id=peer_id, message=wiki_summary, random_id=0)
+        except wikipedia.exceptions.PageError:
+            event.api.messages.send(peer_id=peer_id, message="❌По вашему запросу ничего не найдено❌", random_id=0)
+        except wikipedia.exceptions.DisambiguationError as e:
+            event.api.messages.send(peer_id=peer_id, message="🤔Уточните ваш запрос, найдено несколько подходящих статей🤔", random_id=0)
+
+    return "ok"
