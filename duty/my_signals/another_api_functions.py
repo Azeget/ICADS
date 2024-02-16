d176b9158b9c5f339d9f54fd16110e5b7a6c5db3
Aze <152387619+Azeget@users.noreply.github.com>
2024-02-16 17:55:01 +0300

    Update another_api_functions.py

--- duty/my_signals/another_api_functions.py
+++ duty/my_signals/another_api_functions.py
@@ -8,7 +8,7 @@ from duty.utils import find_mention_by_event, path_from_root
 @dp.my_signal_event_register('группы')
 def groups(event: MySignalEvent) -> str:
     uid = find_mention_by_event(event) or event.db.owner_id
-    message = requests.get(f'http://api.lisi4ka.ru/groups/{uid}').json()['message']  # от ты жопа, пришёл код спиздить?)
+    message = requests.get(f'http://api.lisi4ka.ru/groups/{uid}').json()['message']
     event.edit(message, keep_forward_messages=1)
 
 
@@ -20,8 +20,8 @@ def apps(event: MySignalEvent) -> str:
     event.edit(message, keep_forward_messages=1)
 
 
-@dp.my_signal_event_register('отвязать') # не апи функция, но какая разница где оно лежит?
-def unbind_chat(event: MySignalEvent) -> str: # нахуя оно ток надо?
+@dp.my_signal_event_register('отвязать')
+def unbind_chat(event: MySignalEvent) -> str: 
     e = event.db.chats.pop(event.obj['chat'], None)
     message = 'Чат успешно отвязан!' if e else 'Такого чата уже нет.'
     event.edit(message)
