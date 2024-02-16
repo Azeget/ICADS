2ac693cafaa8671806567ae3c5f029d41ba2c412
Aze <152387619+Azeget@users.noreply.github.com>
2024-02-16 17:47:16 +0300

    Update gp_invited.py

--- duty/callback_signals/gp_invited.py
+++ duty/callback_signals/gp_invited.py
@@ -5,9 +5,9 @@ from time import sleep
 
 
 FAILED_MSG = (
-    '❗ Не получилось назначить группу администратором.\n'
+    '❌ Не удалось  назначить группу администратором.\n'
     'Скорее всего это либо чат сообщества, либо у меня нет'
-    'прав для назначения администраторов'
+    'прав'
 )
 
 
@@ -26,16 +26,16 @@ def groupbot(event: Event):
                         peer_id = msg['peer_id']
                         break
     msg_id = event.api.msg_op(
-        1, peer_id, '👀 Обнаружена группа Ириса, пытаюсь выдать админку...'
+        1, peer_id, '👀 Обнаружена группа Ириса...'
     )
     try:
         if event.api('messages.setMemberRole', peer_id=peer_id,
                      member_id=group_id, role='admin') == 1:
             event.api.msg_op(
-                2, peer_id, '✅ Ириска назначен администратором беседы', msg_id
+                2, peer_id, '✅ Ирис назначен админом', msg_id
             )
     except VkApiResponseException as e:
         if e.error_code == 15:
             event.api.msg_op(2, peer_id, FAILED_MSG, msg_id)
         else:
-            event.api.msg_op(2, peer_id, f'❗ Ошибка VK: {e.error_msg}', msg_id)
+            event.api.msg_op(2, peer_id, f'❗ Ошибка на стороне VK: {e.error_msg}', msg_id)
