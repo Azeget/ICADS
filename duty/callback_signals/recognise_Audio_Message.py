b7151ee16522d297061b31f480a3108f152572dd
Aze <152387619+Azeget@users.noreply.github.com>
2024-02-16 17:49:48 +0300

    Update recognise_Audio_Message.py

--- duty/callback_signals/recognise_Audio_Message.py
+++ duty/callback_signals/recognise_Audio_Message.py
@@ -1,8 +1,5 @@
 from duty.objects import dp, Event
 import time
-# code from:
-# vk: http://vk.com/id194861150
-# github: https://github.com/Alex1249
 @dp.event_register('messages.recogniseAudioMessage')
 def messages_recognise_Audio_Message(event: Event) -> str:
     count=0
@@ -12,11 +9,11 @@ def messages_recognise_Audio_Message(event: Event) -> str:
             peer_id=event.chat.peer_id,
             conversation_message_ids=[event.obj['local_id']])['items'][0]['attachments'][0]['audio_message']['transcript']
             if transcript=='':
-                transcript='Что-то невнятное'
+                transcript='Чего ?'
             break
         except:
             if count>=9:
-                transcript='Время ожидания вышло. Не удалось распознать!'
+                transcript='Время ожидания вышло....'
                 break
             else:
                 time.sleep(0.5)
